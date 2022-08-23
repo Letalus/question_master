4bde2c71fdacbb55452348b5a4d48c2a5025d06d
Chris E <eichmueller.christian@gmail.com>
2022-08-23 03:23:47 +0200

    adjust some translations

--- lib/core/utils/mock_data.dart
+++ lib/core/utils/mock_data.dart
@@ -9,9 +9,9 @@ final kQuizList = <QuizModel>[
   QuizModel(question: AppStrings.due_to_its_simplicity.tr(), image: AssetPaths.image.happyBoy, isCorrect: true),
   QuizModel(
       question: AppStrings.flutter_offers_no_ui.tr(), image: AssetPaths.image.horseLaughing, isCorrect: false),
-  QuizModel(question: AppStrings.flutter_hot_reload.tr(), image: AssetPaths.image.hot, isCorrect: true),
+  QuizModel(question: AppStrings.flutter_hot_reload.tr(), image: AssetPaths.image.hot, isCorrect: false),
   QuizModel(
-      question: AppStrings.flutter_good_community.tr(), image: AssetPaths.image.community, isCorrect: false),
+      question: AppStrings.flutter_good_community.tr(), image: AssetPaths.image.community, isCorrect: true),
   QuizModel(question: AppStrings.flutter_cross_platform.tr(), image: AssetPaths.image.code, isCorrect: true),
   QuizModel(
       question: AppStrings.using_flutter_write_code.tr(), image: AssetPaths.image.platform, isCorrect: false),
@@ -20,7 +20,7 @@ final kQuizList = <QuizModel>[
       image: AssetPaths.image.architecture,
       isCorrect: true),
   QuizModel(
-      question: AppStrings.structure_of_flutter_consist.tr(), image: AssetPaths.image.cplus, isCorrect: false),
+      question: AppStrings.structure_of_flutter_consist.tr(), image: AssetPaths.image.cplus, isCorrect: true),
   QuizModel(
       question: AppStrings.dart_based_platform_takes.tr(), image: AssetPaths.image.platform, isCorrect: false),
   QuizModel(
@@ -30,10 +30,10 @@ final kQuizList = <QuizModel>[
   QuizModel(
       question: AppStrings.flutter_app_development_provides.tr(),
       image: AssetPaths.image.three_d,
-      isCorrect: false),
+      isCorrect: true),
   QuizModel(
-      question: AppStrings.it_is_flutter_ready_to.tr(), image: AssetPaths.image.variety, isCorrect: true),
-  QuizModel(question: AppStrings.since_flutter_is_new.tr(), image: AssetPaths.image.newImg, isCorrect: false),
+      question: AppStrings.it_is_flutter_ready_to.tr(), image: AssetPaths.image.variety, isCorrect: false),
+  QuizModel(question: AppStrings.since_flutter_is_new.tr(), image: AssetPaths.image.newImg, isCorrect: true),
   QuizModel(
       question: AppStrings.dart_is_object_oriented.tr(), image: AssetPaths.image.objectImg, isCorrect: true),
 ];
